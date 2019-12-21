bd67b882cfcf9262cac2c266b6a20dc357aeff59
Kristen Hernandez <kristen.e.hernandez@gmail.com>
2019-12-21 11:05:26 -0500

    jim fixed the server

--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -1,12 +1,11 @@
 // Your server.js file should require the basic npm packages we've used in class: express and path. A lot of this code is borrowed from the Restaurant Table Reservation Activity
 var express = require("express");
-var path = require("path");
 
 //Express configuration
 var app = express();
 
 //port information
-var PORT = process.env.PORT || 8080;
+var PORT = 8080;
 
 //data parsing from the solved restaurant activity
 app.use(express.urlencoded({
